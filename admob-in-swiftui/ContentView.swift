1d9a1dc048de6090ec17b5a162b87ad9c3ab28b4
saitoukun <saitoukun13@gmail.com>
2021-05-05 12:31:25 +0900

    Initial Commit

---
+++ admob-in-swiftui/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  admob-in-swiftui
+//
+//  Created by yohei-saito on 2021/05/05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ admob-in-swiftui/admob_in_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  admob_in_swiftuiApp.swift
+//  admob-in-swiftui
+//
+//  Created by yohei-saito on 2021/05/05.
+//
+
+import SwiftUI
+
+@main
+struct admob_in_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
